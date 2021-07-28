06dc9dbd4718190dc50791ee5a62a94321ddd9fa
binhbill <binhbill95@gmail.com>
2021-07-28 21:28:55 +0700

    PT Bac

---
+++ GIAI PHUONG TRINH BAC 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GIAI_PHUONG_TRINH_BAC_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("nhap a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap b:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap c:");
+            c = int.Parse(Console.ReadLine());
+            int delta = b * b - 4 * a * c;
+            if (delta < 0)
+            {
+                Console.WriteLine("PT da cho Vo Nghiem");
+            }
+            else
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
