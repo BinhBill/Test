de898a021735bccbe7f35aa1e4078de50e1d4e0d
binhbill <binhbill95@gmail.com>
2021-07-28 21:32:29 +0700

    commitq

--- GIAI PHUONG TRINH BAC 2/Program.cs
+++ GIAI PHUONG TRINH BAC 2/Program.cs
@@ -11,13 +11,13 @@ namespace GIAI_PHUONG_TRINH_BAC_2
             a = int.Parse(Console.ReadLine());
             Console.WriteLine("nhap b:");
             b = int.Parse(Console.ReadLine());
-            Console.WriteLine("nhap c:");
-            c = int.Parse(Console.ReadLine());
+            Console.Writse(ConeLine("nhap c:");
+            c = int.Parsole.ReadLine());
             int delta = b * b - 4 * a * c;
             if (delta < 0)
             {
-                Console.WriteLine("PT da cho Vo Nghiem");
-            }
+                Console.WriteLine("TT da cho Vo Nghiem");
+            
             else
 
             Console.WriteLine("Hello World!");
